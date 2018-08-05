8197fa551abb4338d737b3e7d8a7c5400215b531
github username <github email address>
2018-08-05 17:04:35 -0400

    add testing for GET

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,19 @@ const request = require("supertest");
 const { app } = require("./../server")
 const { Todo } = require("./../models/todo")
 
+//add seed data for testing
+const seedTodos = [{
+  text: "First test todo"
+}, {
+  text: "Second test todo"
+}];
+
 //testing lifecycle method
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
-})
+  Todo.remove({}).then(() => {
+    Todo.insertMany(seedTodos);
+  }).then(() => done());
+});
 
 describe("POST /todos", () => {
   it("should create a new todo", (done) => {
@@ -25,7 +34,7 @@ describe("POST /todos", () => {
           return done(error);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -44,10 +53,21 @@ describe("POST /todos", () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((error) => done(error));
       });
   });
+});
 
+describe("GET /todos", () => {
+  it("should get all todos", (done) => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect((response) => {
+        expect(response.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
