682ce17ecc00065498b439ff4ebcdc490dcddbd4
github username <github email address>
2018-08-05 22:44:57 -0400

    fixed bug

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,4 @@
 let mongoose = require("mongoose");
-let { config } = require("./config");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27107/TodoApp");
